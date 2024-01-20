1da8a21c9d5c276407dd52c92993a287f5abd099
Pradeep <pradeepbanjare11@gmail.com>
2024-01-20 17:16:03 +0530

    adding mealSummary inside the meal component

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './MealsSummary.module.css';
+
+export default function MealsSummary() {
+  return (
+    <section className={classes.summary}>
+        <h2>Delicious Food, Deliver To You</h2>
+        <p>Choose your favorite food form our broad selection of available meals and enjoy your delicious lunch or dinner at home</p>
+        <p>All our meals are cooked with high - quality ingredient , just in time and of course by experienced chefs!</p>
+    </section>
+  )
+}
